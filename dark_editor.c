dfc949f56cf3ecc09b817144ec5ac6d09b4e687d
Dark30190 <aleksandrgospodar795@gmail.com>
2026-08-13 23:57:19 +0400

    DarkEditor v0.02: added lines numbers, lines counting and removed fileName's pointer

--- dark_editor.c
+++ dark_editor.c
@@ -4,17 +4,15 @@
 int main() {
 	int stopReading;
 	char fileName[255];
-	char *pFileName[255];
-	// int i = 1;
-	// char lines[5] = "line";
+	int lineCount = 1;
+	int totalLinesWritten = 0;
 	
-	printf("'DarkEditor v0.01'\n");
+	printf("'DarkEditor v0.02'\n"); //descroption
 	printf("this is a testing project.\nThis text editor is written by tg-@Dark30190 and Google Gemini,\nwhile He's learning C.\n");
 	printf("write at telegram,if you see the problems in this programm\nor have ideas to add new features.\n");
 	
 	printf("Enter your file name: "); //accepts a name of file from user
-	scanf("%99s", *pFileName);
-	free(pFileName);
+	scanf("%99s", fileName);
 
 	while ((stopReading = getchar()) != '\n' && stopReading != EOF); //removes the line break from the name
 
@@ -25,20 +23,26 @@ int main() {
 	}
 
 	printf("click Ctrl+D on Linux/macOS or Ctrl+Z on Windows to end tiping.\n");
-	
+
+	printf("%d. ", lineCount); //print a number of first line
+
 	while ((stopReading = getchar()) != EOF) { //writes user's tiping to file
-		// printf("%d. ", i);  				   //adds a line number during tiping(right now it doesn't work currently)
 		fputc(stopReading, input);
-		// i = i + 1;
+
+		if (stopReading == '\n') { //adds a line number during tiping(right now it doesn't work currently)
+			lineCount++;
+			totalLinesWritten++;
+			printf("%d. ", lineCount);
+		}
 	}
 
-	// if (i == 0 || i == 1) {					   //show a number of written lines in file
-	// 	printf("\n%d %s was written to file.\n", i, lines);
-	// } else {
-	// 	printf("\n%d %ss was written to file.\n", i, lines);
-	// }
-	// 
-	fclose(input);	
-
+	fclose(input);
+	
+	if (totalLinesWritten == 1) {					   //show a number of written lines in file
+		printf("\n%d line was written to file.\n", totalLinesWritten);
+	} else {
+		printf("\n%d lines was written to file.\n", totalLinesWritten);
+	}
+	
 	return 0;
 }
